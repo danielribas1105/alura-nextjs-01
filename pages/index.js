a5bef3e90f2e989ba78bebff5288d7335d3d5e8f
Daniel <danielribas1105@gmail.com>
2024-03-30 12:52:36 -0300

    create style project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-import Link from '../src/components/Link';
+import HomeScreen from '../src/screens/HomeScreen';
+
+export default HomeScreen;
+
+/* import Link from '../src/components/Link';
+import Footer from '../src/components/patterns/Footer/index';
+
+function Title({children, as}){
+    const Tag = as;
+    return(
+        <>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`
+                ${Tag}{
+                    color: red;
+                }
+            `}</style>
+        </>
+    )
+}
 
 export default function HomePage(){
     return(
         <div>
-            <h1>Alura Cases - Home</h1>
+            <Title as="h1">Alura Cases - Home</Title>
             <Link href="/faq">Ir para FAQ</Link>
+            <Footer/>
         </div>
     )
-}
+} */
+
+/* https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json */
