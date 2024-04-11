8f23d714d103f2462c60d1a8d5cb70e074c75404
Daniel <danielribas1105@gmail.com>
2024-04-10 21:39:02 -0300

    update project

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,43 @@
+import { useEffect, useState } from 'react';
 import Link from '../src/components/Link';
 
-export default function FaqPage(){
+export async function getStaticProps(context){
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+    return {
+        props: {
+            qualquerCoisaQue: 'eu passar aqui',
+            faq,
+        },
+    };
+}
+
+export default function FaqPage({faq}){
+    console.log(faq);
+   /*  useEffect(() => {
+        const [faq, setFaq] = useState([]);
+    }, []); */
+
     return(
         <div>
             <h1>FAQ</h1>
             <Link href="/">Voltar</Link>
-        </div>
+            <ul>
+                {faq.map(({answer, question}) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
+        </div>  
     )
 }
