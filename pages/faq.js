d0a552f1a800a120b26eb702f584ecd30f019e49
Daniel <danielribas1105@gmail.com>
2024-05-13 22:53:06 -0300

    update project

--- pages/faq.js
+++ pages/faq.js
@@ -1,15 +1,24 @@
-import { useEffect, useState } from 'react';
+import Head from 'next/head';
 import Link from '../src/components/Link';
 
-export async function getStaticProps(context){
+//SSG - Static Site Generation
+//SSR - Server Site Rendering
+//ISG - Incremental Static Generation
+
+//export async function getServerSideProps(){}
+//console.log('Em modo DEV, sempre roda! A cada acesso');
+//console.log('Rodando a cada acesso que vc recebe);
+export async function getStaticProps(){
+    //console.log('Em modo DEV, sempre roda! A cada acesso');
+    //console.log('Rodando SOMENTE em build time);
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
-        .then((respostaDoServidor) => {
-            return respostaDoServidor.json();
-        })
-        .then((resposta) => {
-            return resposta;
-        });
+                    .then((respostaDoServidor) => {
+                    return respostaDoServidor.json();
+                    })
+                    .then((resposta) => {
+                        return resposta;
+                    });
     return {
         props: {
             qualquerCoisaQue: 'eu passar aqui',
@@ -19,13 +28,11 @@ export async function getStaticProps(context){
 }
 
 export default function FaqPage({faq}){
-    console.log(faq);
-   /*  useEffect(() => {
-        const [faq, setFaq] = useState([]);
-    }, []); */
-
     return(
         <div>
+            <Head>
+                <title>Faq - Alura Cases Campanha</title>
+            </Head>
             <h1>FAQ</h1>
             <Link href="/">Voltar</Link>
             <ul>
